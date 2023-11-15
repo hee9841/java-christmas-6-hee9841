969a8492f4811a3ff8ad65e0548f284a3b6635c7
hee9841 <lee98417716@gmail.com>
2023-11-15 22:38:30 +0900

    feat: update customer domain and delete other domains

--- src/main/java/christmas/domain/Customer.java
+++ src/main/java/christmas/domain/Customer.java
@@ -1,18 +1,24 @@
 package christmas.domain;
 
 
-import christmas.Message;
+import christmas.constant.EventType;
+import christmas.constant.Menu;
+import christmas.constant.MenuGroup;
+import christmas.constant.Message;
+import christmas.policy.BenefitPolicy;
+import christmas.policy.EventConditionPolicy;
+import christmas.policy.EventConditionPolicyImpl;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Customer {
 
     private final EnumMap<Menu, Integer> myOrders;
     private final LocalDate visitDate;
-
-    private BigDecimal totalPayment = BigDecimal.ZERO;
 
 
     public Customer(EnumMap<Menu, Integer> myOrders, LocalDate visitDate) {
@@ -20,7 +26,9 @@ public class Customer {
         this.visitDate = visitDate;
     }
 
-    public void calculateTotalPayment() {
+    public BigDecimal calculateTotalPayment() {
+        BigDecimal totalPayment = BigDecimal.ZERO;
+
         for (Map.Entry<Menu, Integer> order : myOrders.entrySet()) {
             Menu menu = order.getKey();
             Integer orderCount = order.getValue();
@@ -28,16 +36,40 @@ public class Customer {
             BigDecimal price = menu.calculatePriceEachMenu(orderCount);
             totalPayment = totalPayment.add(price);
         }
+
+        return totalPayment;
     }
 
     public String findMyOrders() {
         return myOrders.entrySet().stream()
-                .map(entry ->
-                        Message.ORDER_MESSAGE_FORMAT.toFormattedString(entry.getKey().toString(), entry.getValue())
-                )
-                .map(str -> str + "\n")
-                .toString();
+                .map(entry -> Message.COUNT_UNIT_FORMAT.toFormattedString(entry.getKey().toString(), entry.getValue()))
+                .collect(Collectors.joining("\n"));
 
     }
+
+    private EnumMap<MenuGroup, Integer> getEachMenuGroupsCount(EnumMap<Menu, Integer> menus) {
+        EnumMap<MenuGroup, Integer> EachMenuGroupsCount = new EnumMap<>(MenuGroup.class);
+        for (Map.Entry<Menu, Integer> menu : menus.entrySet()) {
+            MenuGroup menuGroup = MenuGroup.findByMenuCode(menu.getKey().name());
+            if (!EachMenuGroupsCount.containsKey(menuGroup)) {
+                EachMenuGroupsCount.put(menuGroup, 0);
+            }
+            EachMenuGroupsCount.put(menuGroup, EachMenuGroupsCount.get(menuGroup) + menu.getValue());
+        }
+
+        return EachMenuGroupsCount;
+    }
+
+    public List<EventType> getEvents() {
+        EventConditionPolicy eventConditionPolicy = new EventConditionPolicyImpl();
+        return eventConditionPolicy.findEvent(visitDate, myOrders, calculateTotalPayment());
+    }
+
+    public EnumMap<EventType, BigDecimal> getBenefits(List<EventType> myEventType) {
+        BenefitPolicy benefitPolicy = new BenefitPolicy();
+        EnumMap<MenuGroup, Integer> myMenuGroups = getEachMenuGroupsCount(myOrders);
+        return benefitPolicy.getMyBenefits(myEventType, visitDate, myMenuGroups);
+    }
+
 
 }
